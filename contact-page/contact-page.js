08158f26d4f2b6c09e1dc7f32b892d8218dae1f6
Georgi Georgiev <137082230+GoshoG123@users.noreply.github.com>
2024-10-29 09:42:03 +0200

    Creating the footer for tablet

--- contact-page/contact-page.js
+++ contact-page/contact-page.js
@@ -1,7 +1,6 @@
 document.getElementById('scrollToTop').addEventListener('click', function(event) {
     event.preventDefault();
     
-    // Check if the page is already at the top
     if (window.scrollY > 0) {
         window.scrollTo({
             top: 0,
